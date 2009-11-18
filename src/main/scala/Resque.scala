38b524b45e71d4fcda383d64f310656461fd0eb9
James Golick <james@giraffesoft.ca>
2009-11-18 14:24:54 -0800

    move the worker registrations in to the Resque object

--- src/main/scala/Resque.scala
+++ src/main/scala/Resque.scala
@@ -1,10 +1,21 @@
 package com.protose.resque
 import com.redis.Redis
 import FancySeq._
+import java.util.Date
 
 class Resque(val redis: Redis, val jobFactory: JobFactory) {
     def reserve(worker: Worker, name: String): Job = {
         jobFactory(worker, name, pop(name))
+    }
+
+    def register(worker: Worker): Unit = {
+        addToWorkersSet(worker)
+        setStartedTime(worker)
+    }
+
+    def unregister(worker: Worker): Unit = {
+        removeFromWorkersSet(worker)
+        deleteStartTime(worker)
     }
 
     protected def pop(name: String): String = {
@@ -12,6 +23,28 @@ class Resque(val redis: Redis, val jobFactory: JobFactory) {
     }
 
     protected def queueName(name: String) = List("resque", "queue", name).join(":")
+
+    protected def setStartedTime(worker: Worker) = {
+        redis.set(startedKey(worker), new Date().toString)
+    }
+
+    protected def startedKey(worker: Worker) = {
+        List("resque:worker", worker.id, "started").join(":")
+    }
+
+    protected def addToWorkersSet(worker: Worker) = {
+        redis.setAdd(workerSet, worker.id)
+    }
+
+    protected def removeFromWorkersSet(worker: Worker) = {
+        redis.setDelete(workerSet, worker.id)
+    }
+
+    protected def deleteStartTime(worker: Worker) = {
+        redis.delete(startedKey(worker))
+    }
+
+    protected def workerSet = "resque:workers"
 }
 
 // vim: set ts=4 sw=4 et:
--- src/test/scala/ResqueSpec.scala
+++ src/test/scala/ResqueSpec.scala
@@ -3,18 +3,21 @@ import org.specs.Specification
 import org.specs.mock.Mockito
 import com.protose.resque._
 import com.redis.Redis
+import FancySeq._
+import java.util.Date
 
 object ResqueSpec extends Specification with Mockito {
     val redis      = mock[Redis]
     val jobFactory = mock[JobFactory]
-    val worker     = mock[Worker]
+    val worker     = new Worker(redis, List("some queue"))
     val job        = Job(worker, "some_queue", "the payload")
-    val queue      = new Resque(redis, jobFactory)
+    val resque     = new Resque(redis, jobFactory)
+    val startKey   = List("resque", "worker", worker.id, "started").join(":")
 
     "reserving a job" in {
         redis.popHead("resque:queue:some_queue") returns "the payload"
         jobFactory.apply(worker, "some_queue", "the payload") returns job
-        val returnVal = queue.reserve(worker, "some_queue")
+        val returnVal = resque.reserve(worker, "some_queue")
 
         "fetches the next payload from the queue" in {
             redis.popHead("resque:queue:some_queue") was called
@@ -23,6 +26,34 @@ object ResqueSpec extends Specification with Mockito {
         "returns the job created with the payload" in {
             jobFactory.apply(worker, "some_queue", "the payload") was called
             returnVal must_== job
+        }
+    }
+
+    "registering a worker" in {
+        val date     = new Date().toString
+        redis.setAdd("resque:workers", worker.id) returns true
+        resque.register(worker)
+
+        "adds the worker to the workers set" in {
+            redis.setAdd("resque:workers", worker.id) was called
+        }
+
+        "informs redis that work has started" in {
+            redis.set(startKey, date) was called
+        }
+    }
+
+    "stopping a worker" in {
+        redis.delete(startKey) returns true
+        redis.setDelete("resque:workers", worker.id) returns true
+        resque.unregister(worker)
+
+        "removes the worker from the workers set" in {
+            redis.setDelete("resque:workers", worker.id) was called
+        }
+
+        "deletes the started time" in {
+            redis.delete(startKey) was called
         }
     }
 }

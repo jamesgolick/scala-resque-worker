2f57884942cd4e6b0a934a5a9f9c248bf0289628
James Golick <james@giraffesoft.ca>
2009-11-18 10:14:01 -0800

    get the pid

--- src/main/scala/Machine.scala
+++ src/main/scala/Machine.scala
@@ -1,8 +1,10 @@
 package com.protose.resque
 import java.net.InetAddress
+import java.lang.management.ManagementFactory
 
 object Machine {
     def hostname = InetAddress.getLocalHost.getHostName
+    def pid = ManagementFactory.getRuntimeMXBean.getName.split("@").first
 }
 
 // vim: set ts=4 sw=4 et:
--- src/test/scala/MachineSpec.scala
+++ src/test/scala/MachineSpec.scala
@@ -4,10 +4,15 @@ import org.specs.mock.Mockito
 import com.protose.resque._
 import com.protose.resque.Machine._
 import java.net.InetAddress
+import java.lang.management.ManagementFactory
 
 object MachineSpec extends Specification with Mockito {
     "returns the hostname" in {
         hostname must_== InetAddress.getLocalHost.getHostName
+    }
+    "returns the pid" in {
+        val expectedPid = ManagementFactory.getRuntimeMXBean.getName.split("@").first
+        pid must_== expectedPid
     }
 }
 
